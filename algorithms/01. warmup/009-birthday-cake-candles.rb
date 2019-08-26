f37b26a0f621386395fb00d3ec5702a07f7a79d4
truongnmt <nguyen.manh.truong@moneyforward.co.jp>
2019-08-26 11:40:10 +0900

    Finish warmup

--- algorithms/01. warmup/009-birthday-cake-candles.rb
+++ algorithms/01. warmup/009-birthday-cake-candles.rb
@@ -1,3 +1,9 @@
 # prob: ar.select{ |x| x == ar.max }.count
 
-ar.select{ |x| x == ar.max }.count
+# ar.select{ |x| x == ar.max }.count
+count_hash = Hash.new(0)
+ar.each do |i|
+    count_hash[i] += 1
+end
+# count_hash.max_by { |k, v| k } #=> [3, 2] key: 3, value: 2
+return count_hash.max[1]
---
+++ algorithms/01. warmup/010-time-conversion.rb
@@ -0,0 +1,4 @@
+# prob: https://www.hackerrank.com/challenges/time-conversion/problem
+
+dt = DateTime.parse(s)
+dt.strftime('%H:%M:%S')
